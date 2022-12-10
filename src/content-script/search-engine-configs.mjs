86e335beb8f957d6c165822d749114b39e22f018
Jos Storer <joshua1466587594@outlook.com>
2022-12-10 15:03:58 +0800

    compatibility improvement for baidu

---
+++ src/content-script/search-engine-configs.mjs
@@ -0,0 +1,103 @@
+import { run, getSearchInputValue } from './index.mjs'
+
+const init = {
+  baidu: () => {
+    const targetNode = document.getElementById('wrapper_wrapper')
+    const callback = (records) => {
+      if (
+        records.some(
+          (record) =>
+            record.type === 'childList' &&
+            [...record.addedNodes].some((node) => node.id === 'container'),
+        )
+      ) {
+        const searchValue = getSearchInputValue(config.baidu)
+        if (searchValue) {
+          run(searchValue, config.baidu)
+        }
+      }
+    }
+    const observer = new MutationObserver(callback)
+    observer.observe(targetNode, { childList: true })
+  },
+}
+
+/**
+ * @typedef {object} SiteConfigAction
+ * @property {function} init
+ */
+/**
+ * @typedef {object} SiteConfig
+ * @property {string[]} inputQuery - for search box
+ * @property {string[]} sidebarContainerQuery - prepend child to
+ * @property {string[]} appendContainerQuery - if sidebarContainer not exists, append child to
+ * @property {SiteConfigAction} action
+ */
+/**
+ * @type {Object.<string,SiteConfig>}
+ */
+export const config = {
+  google: {
+    inputQuery: ["input[name='q']"],
+    sidebarContainerQuery: ['#rhs'],
+    appendContainerQuery: ['#rcnt'],
+  },
+  bing: {
+    inputQuery: ["textarea[name='q']"],
+    sidebarContainerQuery: ['#b_context'],
+    appendContainerQuery: [],
+  },
+  yahoo: {
+    inputQuery: ["input[name='p']"],
+    sidebarContainerQuery: ['#right', '.Contents__inner.Contents__inner--sub'],
+    appendContainerQuery: ['#cols', '#contents__wrap'],
+  },
+  duckduckgo: {
+    inputQuery: ["input[name='q']"],
+    sidebarContainerQuery: ['.results--sidebar.js-results-sidebar'],
+    appendContainerQuery: ['#links_wrapper'],
+  },
+  startpage: {
+    inputQuery: ["input[name='query']"],
+    sidebarContainerQuery: ['.layout-web__sidebar.layout-web__sidebar--web'],
+    appendContainerQuery: ['.layout-web__body.layout-web__body--desktop'],
+  },
+  baidu: {
+    inputQuery: ["input[name='wd']"],
+    sidebarContainerQuery: ['#content_right'],
+    appendContainerQuery: ['#container'],
+    action: {
+      init: init.baidu,
+    },
+  },
+  kagi: {
+    inputQuery: ["input[name='q']"],
+    sidebarContainerQuery: ['.right-content-box._0_right_sidebar'],
+    appendContainerQuery: ['#_0_app_content'],
+  },
+  yandex: {
+    inputQuery: ["input[name='text']"],
+    sidebarContainerQuery: ['#search-result-aside'],
+    appendContainerQuery: [],
+  },
+  naver: {
+    inputQuery: ["input[name='query']"],
+    sidebarContainerQuery: ['#sub_pack'],
+    appendContainerQuery: ['#content'],
+  },
+  brave: {
+    inputQuery: ["input[name='q']"],
+    sidebarContainerQuery: ['#side-right'],
+    appendContainerQuery: [],
+  },
+  searx: {
+    inputQuery: ["input[name='q']"],
+    sidebarContainerQuery: ['#sidebar_results'],
+    appendContainerQuery: [],
+  },
+  ecosia: {
+    inputQuery: ["input[name='q']"],
+    sidebarContainerQuery: ['.sidebar.web__sidebar'],
+    appendContainerQuery: ['#main'],
+  },
+}
