3d787c63b328a3330ce21fa0dab3e86133a5e307
Jos Storer <joshua1466587594@outlook.com>
2022-12-13 15:06:02 +0800

    update the style of link

--- src/content-script/markdown.jsx
+++ src/content-script/markdown.jsx
@@ -6,13 +6,21 @@ import remarkMath from 'remark-math'
 import remarkGfm from 'remark-gfm'
 
 export function MarkdownRender(props) {
-  const linkProperties = { target: '_blank', rel: 'nofollow noopener noreferrer' }
+  const linkProperties = {
+    target: '_blank',
+    style: 'color: #8ab4f8',
+    rel: 'nofollow noopener noreferrer',
+  }
   return (
     <ReactMarkdown
       remarkPlugins={[remarkMath, remarkGfm]}
       rehypePlugins={[rehypeKatex, rehypeRaw, rehypeHighlight]}
       components={{
-        a: props => <a href={ props.href } { ...linkProperties }>{ props.children }</a>
+        a: (props) => (
+          <a href={props.href} {...linkProperties}>
+            {props.children}
+          </a>
+        ),
       }}
       {...props}
     >
