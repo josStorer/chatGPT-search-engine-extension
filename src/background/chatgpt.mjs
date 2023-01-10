6ddaf065107395cab878f15463c221cfc64eab9c
josc146 <josstorer@outlook.com>
2023-01-10 18:15:58 +0800

    chore: add background request debug messages

--- src/background/chatgpt.mjs
+++ src/background/chatgpt.mjs
@@ -1,5 +1,5 @@
 async function request(token, method, path, data) {
-  return fetch(`https://chat.openai.com/backend-api${path}`, {
+  const response = await fetch(`https://chat.openai.com/backend-api${path}`, {
     method,
     headers: {
       'Content-Type': 'application/json',
@@ -7,6 +7,9 @@ async function request(token, method, path, data) {
     },
     body: JSON.stringify(data),
   })
+  const responseText = await response.text()
+  console.debug(`request: ${path}`, responseText)
+  return { response, responseText }
 }
 
 export async function sendMessageFeedback(token, data) {
@@ -15,4 +18,13 @@ export async function sendMessageFeedback(token, data) {
 
 export async function setConversationProperty(token, conversationId, propertyObject) {
   await request(token, 'PATCH', `/conversation/${conversationId}`, propertyObject)
+}
+
+export async function sendModerations(token, question, conversationId, messageId) {
+  await request(token, 'POST', `/moderations`, {
+    conversation_id: conversationId,
+    input: question,
+    message_id: messageId,
+    model: 'text-moderation-playground',
+  })
 }
