5492f7bd2dfc471e01bb811f5ef5fe0328afd587
josc146 <josstorer@outlook.com>
2023-01-13 12:31:01 +0800

    ci: add mobile verify

--- .github/workflows/scripts/verify-search-engine-configs.mjs
+++ .github/workflows/scripts/verify-search-engine-configs.mjs
@@ -2,15 +2,6 @@ import { JSDOM } from 'jsdom'
 import { config } from '../../../src/content-script/search-engine-configs.mjs'
 import fetch, { Headers } from 'node-fetch'
 
-let headers = new Headers({
-  'User-Agent':
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/108.0.1462.76',
-  Accept:
-    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-  Connection: 'keep-alive',
-  'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7', // for baidu
-})
-
 const urls = {
   google: ['https://www.google.com/search?q=hello'],
   bing: ['https://www.bing.com/search?q=hello', 'https://cn.bing.com/search?q=hello'],
@@ -26,16 +17,37 @@ const urls = {
   ecosia: [], // unknown verify method https://www.ecosia.org/search?q=hello
 }
 
-const queryNames = [
+const commonHeaders = {
+  Accept:
+    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  Connection: 'keep-alive',
+  'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7', // for baidu
+}
+
+const desktopHeaders = new Headers({
+  'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/108.0.1462.76',
+  ...commonHeaders,
+})
+
+const mobileHeaders = {
+  'User-Agent':
+    'Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36 Edg/108.0.1462.76',
+  ...commonHeaders,
+}
+
+const desktopQueryNames = [
   'inputQuery',
   'sidebarContainerQuery',
   'appendContainerQuery',
   'resultsContainerQuery',
 ]
 
+const mobileQueryNames = ['inputQuery', 'resultsContainerQuery']
+
 let errors = ''
 
-async function verify() {
+async function verify(errorTag, urls, headers, queryNames) {
   await Promise.all(
     Object.entries(urls).map(([siteName, urlArray]) =>
       Promise.all(
@@ -49,30 +61,41 @@ async function verify() {
               const dom = new JSDOM(text)
               for (const queryName of queryNames) {
                 const queryArray = config[siteName][queryName]
-                if (queryArray.length > 0)
-                  if (
-                    queryArray.some((query) => {
-                      const section = dom.window.document.querySelector(query)
-                      if (section) return true
-                    })
-                  ) {
-                    console.log(`${siteName} ${url} ${queryName} passed`)
-                  } else {
-                    const error = `${siteName} ${url} ${queryName} failed`
-                    errors += error + '\n'
+                if (queryArray.length === 0) continue
+
+                let foundQuery
+                for (const query of queryArray) {
+                  const element = dom.window.document.querySelector(query)
+                  if (element) {
+                    foundQuery = query
+                    break
                   }
+                }
+                if (foundQuery) {
+                  console.log(`${siteName} ${url} ${queryName}: ${foundQuery} passed`)
+                } else {
+                  const error = `${siteName} ${url} ${queryName} failed`
+                  errors += errorTag + error + '\n'
+                }
               }
             })
             .catch((error) => {
-              errors += error + '\n'
+              errors += errorTag + error + '\n'
             }),
         ),
       ),
     ),
   )
+}
+
+async function main() {
+  console.log('Verify desktop search engine configs:')
+  await verify('desktop: ', urls, desktopHeaders, desktopQueryNames)
+  console.log('\nVerify mobile search engine configs:')
+  await verify('mobile: ', urls, mobileHeaders, mobileQueryNames)
 
   if (errors.length > 0) throw new Error('\n' + errors)
   else console.log('\nAll passed')
 }
 
-verify()
+main()
