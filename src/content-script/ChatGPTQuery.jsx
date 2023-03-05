d36fcc2905edfe50af5c06b655a5c628982ead57
josc146 <josStorer@outlook.com>
2023-03-05 20:08:03 +0800

    feat: multiple lines text input, `enter` to send, `shift + enter` to break line

--- src/content-script/ChatGPTQuery.jsx
+++ src/content-script/ChatGPTQuery.jsx
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from 'react'
+import { memo, useEffect, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import { MarkdownRender } from './markdown.jsx'
 import Browser from 'webextension-polyfill'
@@ -81,31 +81,42 @@ TalkItem.propTypes = {
 
 function Interact({ onSubmit, enabled }) {
   const [value, setValue] = useState('')
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    inputRef.current.style.height = 'auto'
+    const computed = window.getComputedStyle(inputRef.current)
+    const height =
+      parseInt(computed.getPropertyValue('border-top-width'), 10) +
+      parseInt(computed.getPropertyValue('padding-top'), 10) +
+      inputRef.current.scrollHeight +
+      parseInt(computed.getPropertyValue('padding-bottom'), 10) +
+      parseInt(computed.getPropertyValue('border-bottom-width'), 10)
+
+    inputRef.current.style.height = `${height}px`
+  })
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13 && e.shiftKey === false) {
+      e.preventDefault()
+      if (!value) return
+      onSubmit(value)
+      setValue('')
+    }
+  }
 
   return (
-    <form
-      className="interact-container"
-      id="interact"
-      onSubmit={(e) => {
-        e.preventDefault()
-        if (!value) return
-        onSubmit(value)
-        setValue('')
-      }}
-    >
-      <input
-        disabled={!enabled}
-        className="interact-input"
-        type="text"
-        placeholder={
-          enabled
-            ? 'Type your question here'
-            : 'Wait for the answer to finish and then continue here'
-        }
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-    </form>
+    <textarea
+      ref={inputRef}
+      disabled={!enabled}
+      className="interact-input"
+      placeholder={
+        enabled ? 'Type your question here' : 'Wait for the answer to finish and then continue here'
+      }
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      onKeyDown={onKeyDown}
+    />
   )
 }
 
