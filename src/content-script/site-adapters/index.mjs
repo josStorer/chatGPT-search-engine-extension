f912e70c62a21454271710c723cc84ba7ab2606f
josc146 <josStorer@outlook.com>
2023-03-10 15:58:10 +0800

    refactor: move init.baidu to site-adapters

--- src/content-script/site-adapters/index.mjs
+++ src/content-script/site-adapters/index.mjs
@@ -1,27 +1,4 @@
-const init = {
-  baidu: async (hostname, userConfig, getInput, mountComponent) => {
-    try {
-      const targetNode = document.getElementById('wrapper_wrapper')
-      const observer = new MutationObserver(async (records) => {
-        if (
-          records.some(
-            (record) =>
-              record.type === 'childList' &&
-              [...record.addedNodes].some((node) => node.id === 'container'),
-          )
-        ) {
-          const searchValue = await getInput(config.baidu.inputQuery)
-          if (searchValue) {
-            mountComponent(config.baidu, userConfig)
-          }
-        }
-      })
-      observer.observe(targetNode, { childList: true })
-    } catch (e) {
-      /* empty */
-    }
-  },
-}
+import baidu from './baidu'
 
 /**
  * @typedef {object} SiteConfigAction
@@ -80,7 +57,7 @@ export const config = {
     appendContainerQuery: ['#container'],
     resultsContainerQuery: ['#content_left', '#results'],
     action: {
-      init: init.baidu,
+      init: baidu.init,
     },
   },
   kagi: {
