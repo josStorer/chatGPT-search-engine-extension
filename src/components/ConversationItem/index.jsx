67f654b264f8b9778befc59bfa0a747ce6f70ce2
josc146 <josStorer@outlook.com>
2023-03-10 20:03:44 +0800

    feat: improve display effect and add copy, collapse button for question and error (#26)

--- src/components/ConversationItem/index.jsx
+++ src/components/ConversationItem/index.jsx
@@ -8,45 +8,88 @@ import MarkdownRender from '../MarkdownRender/markdown.jsx'
 export function ConversationItem({ type, content, session, done }) {
   const [collapsed, setCollapsed] = useState(false)
 
-  return (
-    <div className={type} dir="auto">
-      {type === 'answer' && (
-        <div className="gpt-header">
-          <p>{session ? 'ChatGPT:' : 'Loading...'}</p>
-          <div style="display: flex; gap: 15px;">
-            {done && !session.useApiKey && (
-              <FeedbackForChatGPTWeb
-                messageId={session.messageId}
-                conversationId={session.conversationId}
-              />
-            )}
-            {session && session.conversationId && !session.useApiKey && (
-              <a
-                title="Continue on official website"
-                href={'https://chat.openai.com/chat/' + session.conversationId}
-                target="_blank"
-                rel="nofollow noopener noreferrer"
-                style="color: inherit;"
-              >
-                <LinkExternalIcon size={14} />
-              </a>
-            )}
-            {session && <CopyButton contentFn={() => content} size={14} />}
-            {!collapsed ? (
-              <span title="Collapse" className="gpt-util-icon" onClick={() => setCollapsed(true)}>
-                <XCircleIcon size={14} />
-              </span>
-            ) : (
-              <span title="Expand" className="gpt-util-icon" onClick={() => setCollapsed(false)}>
-                <ChevronDownIcon size={14} />
-              </span>
-            )}
+  switch (type) {
+    case 'question':
+      return (
+        <div className={type} dir="auto">
+          <div className="gpt-header">
+            <p>You:</p>
+            <div style="display: flex; gap: 15px;">
+              <CopyButton contentFn={() => content} size={14} />
+              {!collapsed ? (
+                <span title="Collapse" className="gpt-util-icon" onClick={() => setCollapsed(true)}>
+                  <XCircleIcon size={14} />
+                </span>
+              ) : (
+                <span title="Expand" className="gpt-util-icon" onClick={() => setCollapsed(false)}>
+                  <ChevronDownIcon size={14} />
+                </span>
+              )}
+            </div>
           </div>
+          {!collapsed && <MarkdownRender>{content}</MarkdownRender>}
         </div>
-      )}
-      {!collapsed && <MarkdownRender>{content}</MarkdownRender>}
-    </div>
-  )
+      )
+    case 'answer':
+      return (
+        <div className={type} dir="auto">
+          <div className="gpt-header">
+            <p>{session ? 'ChatGPT:' : 'Loading...'}</p>
+            <div style="display: flex; gap: 15px;">
+              {done && !session.useApiKey && (
+                <FeedbackForChatGPTWeb
+                  messageId={session.messageId}
+                  conversationId={session.conversationId}
+                />
+              )}
+              {session && session.conversationId && !session.useApiKey && (
+                <a
+                  title="Continue on official website"
+                  href={'https://chat.openai.com/chat/' + session.conversationId}
+                  target="_blank"
+                  rel="nofollow noopener noreferrer"
+                  style="color: inherit;"
+                >
+                  <LinkExternalIcon size={14} />
+                </a>
+              )}
+              {session && <CopyButton contentFn={() => content} size={14} />}
+              {!collapsed ? (
+                <span title="Collapse" className="gpt-util-icon" onClick={() => setCollapsed(true)}>
+                  <XCircleIcon size={14} />
+                </span>
+              ) : (
+                <span title="Expand" className="gpt-util-icon" onClick={() => setCollapsed(false)}>
+                  <ChevronDownIcon size={14} />
+                </span>
+              )}
+            </div>
+          </div>
+          {!collapsed && <MarkdownRender>{content}</MarkdownRender>}
+        </div>
+      )
+    case 'error':
+      return (
+        <div className={type} dir="auto">
+          <div className="gpt-header">
+            <p>Error:</p>
+            <div style="display: flex; gap: 15px;">
+              <CopyButton contentFn={() => content} size={14} />
+              {!collapsed ? (
+                <span title="Collapse" className="gpt-util-icon" onClick={() => setCollapsed(true)}>
+                  <XCircleIcon size={14} />
+                </span>
+              ) : (
+                <span title="Expand" className="gpt-util-icon" onClick={() => setCollapsed(false)}>
+                  <ChevronDownIcon size={14} />
+                </span>
+              )}
+            </div>
+          </div>
+          {!collapsed && <MarkdownRender>{content}</MarkdownRender>}
+        </div>
+      )
+  }
 }
 
 ConversationItem.propTypes = {
