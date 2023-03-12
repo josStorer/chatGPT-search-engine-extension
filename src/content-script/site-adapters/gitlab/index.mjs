ba992396d7c714d2e33348b0493d7e3e64fbe498
josc146 <josStorer@outlook.com>
2023-03-12 19:57:36 +0800

    feat: git patch summary for gitlab commit

---
+++ src/content-script/site-adapters/gitlab/index.mjs
@@ -0,0 +1,34 @@
+import { cropText } from '../../../utils/crop-text.mjs'
+import { limitedFetch } from '../../../utils/limited-fetch.mjs'
+
+const getPatchUrl = async () => {
+  const patchUrl = location.origin + location.pathname + '.patch'
+  const response = await fetch(patchUrl, { method: 'HEAD' })
+  if (response.ok) return patchUrl
+  return ''
+}
+
+const getPatchData = async (patchUrl) => {
+  if (!patchUrl) return
+
+  let patchData = await limitedFetch(patchUrl, 1024 * 40)
+  patchData = patchData.substring(patchData.indexOf('---'))
+  return patchData
+}
+
+export default {
+  inputQuery: async () => {
+    try {
+      const patchUrl = await getPatchUrl()
+      const patchData = await getPatchData(patchUrl)
+      if (!patchData) return
+
+      return cropText(
+        `Analyze the contents of a git commit,provide a suitable commit message,and summarize the contents of the commit.` +
+          `The patch contents of this commit are as follows:\n${patchData}`,
+      )
+    } catch (e) {
+      console.log(e)
+    }
+  },
+}
