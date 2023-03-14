5f90e25a241529966b2c4aa04b05b886374e22ff
josc146 <josStorer@outlook.com>
2023-03-14 21:49:31 +0800

    chore: fix return value size of `limitedFetch`

--- src/utils/limited-fetch.mjs
+++ src/utils/limited-fetch.mjs
@@ -6,11 +6,11 @@ export async function limitedFetch(url, maxBytes) {
       const xhr = new XMLHttpRequest()
       xhr.onprogress = (ev) => {
         if (ev.loaded < maxBytes) return
-        resolve(ev.target.responseText)
+        resolve(ev.target.responseText.substring(0, maxBytes))
         xhr.abort()
       }
       xhr.onload = (ev) => {
-        resolve(ev.target.responseText)
+        resolve(ev.target.responseText.substring(0, maxBytes))
       }
       xhr.onerror = (ev) => {
         reject(new Error(ev.target.status))
